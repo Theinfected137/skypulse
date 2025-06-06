6a1f249cda215c0af79b85bfa7d7119c25e15974
Leonor Oliveira <leonor.mm.oliveira@datapoint.pt>
2025-06-06 11:12:32 +0000

    feat: app: add weather here

--- SkyPulse/src/app/app.ts
+++ SkyPulse/src/app/app.ts
@@ -2,14 +2,27 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Weather } from './weather/weather';
+import { WeatherService } from './weather';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, Weather],
+  imports: [CommonModule, RouterOutlet, Weather, FormsModule, HttpClientModule],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
 export class App{
-  title = 'SkyPulse';
+  city = '';
+  weatherData: any;
+
+  constructor(private weatherService: WeatherService) {}
+
+  searchWeather() {
+    this.weatherService.getWeather(this.city).subscribe(data => {
+      this.weatherData = data;
+      console.log(data);
+    });
+  }
 }
